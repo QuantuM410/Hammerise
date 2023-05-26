d84301319d5a6e46369514c851c93ae40da38fac
Kartikey Singh <kartikeys410@gmail.com>
2023-05-26 18:57:51 +0530

    resets react and tailwind with components

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
 import React from "react";
-import LandingPage from "./My_app/frontend/pages/landing_page/landing_page";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import LoginPage from "./My_app/frontend/pages/login_page/login_page.js";
-import SignupPage from "./My_app/frontend/pages/signup_page/signup_page.js";
 
-const App = () => { 
-    return (
-      <Router>
-        <Routes>
-          <Route path="/" element={<LoginPage />} />
-          <Route path="/landingpage" element={<LandingPage />} />
-          <Route path="/signup" element={<SignupPage />} />
-        </Routes>
-      </Router>
-  
-    );
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-center">Hello</h1>
+    </div>
+  );
 }
 
 export default App;
