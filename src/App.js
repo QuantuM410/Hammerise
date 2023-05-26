d75952a2b458fdebcd86c2f839d9c8a0b8e46a11
Kartikey Singh <kartikeys410@gmail.com>
2023-05-26 17:11:08 +0530

    recreates react app without vite, sets up tailwind

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Login from "./Components/Login_Page/Login";
+function App() {
+  return (
+    <div className="App">
+      <Login />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Login_Page/Login.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Login = () => {
+    return (
+        <div className="text-white">
+            <h1 className="text-center">HELLO WORLD</h1>
+        </div>
+    )
+}
+
+export default Login;
