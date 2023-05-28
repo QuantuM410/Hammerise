0c21d061cbebca59559adb355a85443d20738ede
Kartikey Singh <kartikeys410@gmail.com>
2023-05-28 14:13:02 +0530

    adds component files

---
+++ src/components/home_page/home.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Home = () => {
+    return (
+        <div className="w-full h-screen  flex bg-gradient-to-r from-gray-900 to-gray-500 ">
+
+
+
+        </div>
+
+
+    )
+
+
+}
+
+export default Home;
