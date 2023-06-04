7b7d02d6e3188b2a3ed70e3350f7aa87618291e7
Kartikey Singh <kartikeys410@gmail.com>
2023-06-04 16:06:53 +0530

    <added>:Registration page

---
+++ src/components/registration_page/ImageUpload/ImageUpload.jsx
@@ -0,0 +1,62 @@
+import React, { useState, useRef } from "react";
+import { AiOutlineCloudUpload } from "react-icons/ai";
+
+function ImageUpload() {
+    const [uploadedImage, setUploadedImage] = useState(null);
+    const fileInputRef = useRef(null);
+
+    const handleImageUpload = (event) => {
+        const file = event.target.files[0];
+        const reader = new FileReader();
+
+        reader.onloadend = () => {
+            setUploadedImage(reader.result);
+        };
+
+        if (file) {
+            reader.readAsDataURL(file);
+        }
+    };
+
+    const handleButtonClick = () => {
+        fileInputRef.current.click();
+    };
+
+    const containerStyle = uploadedImage
+        ? {
+            backgroundImage: `url(${uploadedImage})`,
+        }
+        : {};
+
+    return (
+        <div className="flex flex-col mx-auto  mt-4 items-center">
+            <div
+                className="w-96 h-96 border-4 border-black rounded-2xl bg-cover bg-center bg-no-repeat"
+                style={containerStyle}
+            >
+                {!uploadedImage && (
+                    <div className="flex items-center justify-center h-full">
+                        <AiOutlineCloudUpload size={100} color="black" />
+                    </div>
+                )}
+
+                <input
+                    type="file"
+                    accept="image/*"
+                    onChange={handleImageUpload}
+                    className="hidden"
+                    ref={fileInputRef}
+                />
+            </div>
+
+            <button
+                onClick={handleButtonClick}
+                className="mt-4 px-8 py-4 bg-gray-200 border-none rounded-xl cursor-pointer font-quicksand font-bold text-white text-2xl bg-secondary opacity-80"
+            >
+                Upload Image
+            </button>
+        </div>
+    );
+}
+
+export default ImageUpload;
--- src/components/registration_page/registration.jsx
+++ src/components/registration_page/registration.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import registrationbg from "../../assets/regbg.jpeg";
-import Dropdown2 from './filter_dropdown';
+import Dropdown2 from './Dropdown/filter_dropdown';
+import ImageUpload from './ImageUpload/ImageUpload';
 
 function Registration() {
     const [locationRequired, setLocationRequired] = useState(false);
@@ -17,59 +18,66 @@ function Registration() {
                 style={{ backgroundImage: `url(${registrationbg})`, zIndex: 0 }}
             ></div>
             <div className="flex items-center justify-center h-full relative z-10">
-                <div className="h-3/5 w-3/5 bg-[#F1D6AC] bg-opacity-80  p-8 rounded-3xl shadow-lg flex flex-col">
+                <div className="h-3/5 w-[1200px] bg-[#F1D6AC] bg-opacity-80  p-8 rounded-3xl shadow-lg flex flex-col">
                     <h1 className="flex justify-center items-center font-quicksand text-5xl font-bold text-black">Sign Up</h1>
                     <div className="border-4 w-20 border-black mx-auto my-6" />
-                    <input
-                        type="email"
-                        placeholder="Email"
-                        className="ml-10 w-[600px] bg-[#000000] opacity-70 text-lg font-quicksand text-white py-6 border-b border-white outline-none focus:outline-none px-4 my-4 bg-transparent rounded-2xl transition-transform duration-300 focus:scale-105"
-                    />
-                    <input
-                        type="password"
-                        placeholder="Password"
-                        className="ml-10 w-[600px] bg-[#000000] opacity-70 text-lg font-quicksand text-white py-5 border-b border-white outline-none focus:outline-none px-4 my-4 bg-transparent rounded-2xl transition-transform duration-300 focus:scale-105"
-                    />
-                    <input
-                        type="password"
-                        placeholder="Confirm Password"
-                        className="ml-10 w-[600px] bg-[#000000] opacity-70 text-lg font-quicksand text-white py-5 border-b border-white outline-none focus:outline-none px-4 my-4 bg-transparent rounded-2xl transition-transform duration-300 focus:scale-105"
-                    />
-                    <div className="flex ml-10">
-                        <input
-                            type="text"
-                            placeholder="Street"
-                            className="w-[290px] bg-[#000000] opacity-70 text-lg font-quicksand text-white py-5 border-b border-white outline-none focus:outline-none px-4 my-4 bg-transparent rounded-2xl transition-transform duration-300 focus:scale-105 mr-2"
-                        />
-                        <input
-                            type="text"
-                            placeholder="Area"
-                            className="w-[300px] bg-[#000000] opacity-70 text-lg font-quicksand text-white py-5 border-b border-white outline-none focus:outline-none px-4 my-4 bg-transparent rounded-2xl transition-transform duration-300 focus:scale-105 mr-2"
-                        />
-                    </div>
-                    <div className="flex ml-10">
-                        <input
-                            type="text"
-                            placeholder="District"
-                            className="w-[290px] bg-[#000000] opacity-70 text-lg font-quicksand text-white py-5 border-b border-white outline-none focus:outline-none px-4 my-4 bg-transparent rounded-2xl transition-transform duration-300 focus:scale-105 mr-2"
-                        />
-                        <input
-                            type="text"
-                            placeholder="State"
-                            className="w-[300px] bg-[#000000] opacity-70 text-lg font-quicksand text-white py-5 border-b border-white outline-none focus:outline-none px-4 my-4 bg-transparent rounded-2xl transition-transform duration-300 focus:scale-105"
-                        />
-                    </div>
+                    <div className='flex'>
+                        <div className='flex ml-0 mx-auto flex-col'>
+                            <input
+                                type="email"
+                                placeholder="Email"
+                                autoComplete="off"
+                                className="ml-10 w-[600px] bg-[#000000] opacity-70 text-lg font-quicksand text-white py-6 border-b border-white outline-none focus:outline-none px-4 my-4 bg-transparent rounded-2xl transition-transform duration-300 focus:scale-105"
+                            />
+                            <input
+                                type="password"
+                                autoComplete="off"
+                                placeholder="Password"
+                                className="ml-10 w-[600px] bg-[#000000] opacity-70 text-lg font-quicksand text-white py-5 border-b border-white outline-none focus:outline-none px-4 my-4 bg-transparent rounded-2xl transition-transform duration-300 focus:scale-105"
+                            />
+                            <input
+                                type="password"
+                                placeholder="Confirm Password"
+                                className="ml-10 w-[600px] bg-[#000000] opacity-70 text-lg font-quicksand text-white py-5 border-b border-white outline-none focus:outline-none px-4 my-4 bg-transparent rounded-2xl transition-transform duration-300 focus:scale-105"
+                            />
+                            <div className="flex ml-10">
+                                <input
+                                    type="text"
+                                    placeholder="Street"
+                                    className="w-[290px] bg-[#000000] opacity-70 text-lg font-quicksand text-white py-5 border-b border-white outline-none focus:outline-none px-4 my-4 bg-transparent rounded-2xl transition-transform duration-300 focus:scale-105 mr-2"
+                                />
+                                <input
+                                    type="text"
+                                    placeholder="Area"
+                                    className="w-[300px] bg-[#000000] opacity-70 text-lg font-quicksand text-white py-5 border-b border-white outline-none focus:outline-none px-4 my-4 bg-transparent rounded-2xl transition-transform duration-300 focus:scale-105 mr-2"
+                                />
+                            </div>
+                            <div className="flex ml-10">
+                                <input
+                                    type="text"
+                                    placeholder="District"
+                                    className="w-[290px] bg-[#000000] opacity-70 text-lg font-quicksand text-white py-5 border-b border-white outline-none focus:outline-none px-4 my-4 bg-transparent rounded-2xl transition-transform duration-300 focus:scale-105 mr-2"
+                                />
+                                <input
+                                    type="text"
+                                    placeholder="State"
+                                    className="w-[300px] bg-[#000000] opacity-70 text-lg font-quicksand text-white py-5 border-b border-white outline-none focus:outline-none px-4 my-4 bg-transparent rounded-2xl transition-transform duration-300 focus:scale-105"
+                                />
+                            </div>
 
-                    <Dropdown2 />
-                    <div className="flex items-center ml-10 mt-10 my-4">
-                        <input
-                            type="checkbox"
-                            id="locationCheckbox"
-                            className="form-checkbox h-10 w-5  text-[#000000] rounded-md bg-[#000000] transition duration-200 ease-in-out "
-                            checked={locationRequired}
-                            onChange={handleLocationChange}
-                        />
-                        <label htmlFor="locationCheckbox" className="ml-2 font-quicksand  font-bold text-xl text-black">This product requires your location</label>
+                            <Dropdown2 />
+                            <div className="flex items-center ml-10 mt-10 my-4">
+                                <input
+                                    type="checkbox"
+                                    id="locationCheckbox"
+                                    className="form-checkbox h-10 w-5  text-[#000000] rounded-md bg-[#000000] transition duration-200 ease-in-out "
+                                    checked={locationRequired}
+                                    onChange={handleLocationChange}
+                                />
+                                <label htmlFor="locationCheckbox" className="ml-2 font-quicksand  font-bold text-xl text-black">This product requires your location</label>
+                            </div>
+                        </div>
+                        <ImageUpload />
                     </div>
                 </div>
             </div>
